3e1b07529717ddb490dc08c80e81b917620588a6
steller21 <sujithkumar69101@gmail.com>
2026-03-16 11:11:10 +0530

    Project Complete

--- CodeAlpha_CALCULATOR/script.js
+++ CodeAlpha_CALCULATOR/script.js
@@ -6,6 +6,11 @@ function clearDisplay() {
     document.getElementById("display").value = "";
 }
 
+function deleteLast() {
+    let currentDisplay = document.getElementById("display").value;
+    document.getElementById("display").value = currentDisplay.slice(0, -1);
+}
+
 function calculate() {
     try {
         document.getElementById("display").value =
